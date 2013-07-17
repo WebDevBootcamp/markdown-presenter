1742dd17a5b092b640678416c3f45b746d7dda51
Matt Kunze <matt.kunze@datasplice.com>
2013-07-16 23:12:55 -0600

    hack together initial bootswatch style
    would be better to use the source less files for both bootstrap and
    bootswatch, but this will work for now
    
    use font-awesome for icons instead of glyphicons

--- deploy.sh
+++ deploy.sh
@@ -11,10 +11,13 @@ cp www/js/* deploy/js
 
 mkdir -p deploy/vendor/bootstrap.css/css
 mkdir -p deploy/vendor/bootstrap.css/js
-mkdir -p deploy/vendor/bootstrap.css/img
 cp www/vendor/bootstrap.css/css/* deploy/vendor/bootstrap.css/css
 cp www/vendor/bootstrap.css/js/* deploy/vendor/bootstrap.css/js
-cp www/vendor/bootstrap.css/img/* deploy/vendor/bootstrap.css/img
+
+mkdir -p deploy/vendor/font-awesome/css
+mkdir -p deploy/vendor/font-awesome/font
+cp www/vendor/font-awesome/css/* deploy/vendor/font-awesome/css
+cp www/vendor/font-awesome/font/* deploy/vendor/font-awesome/font
 
 mkdir -p deploy/vendor/ace-builds/src
 cp www/vendor/ace-builds/src/* deploy/vendor/ace-builds/src
