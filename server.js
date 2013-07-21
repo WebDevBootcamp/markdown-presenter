a46e4f8a06cb1ce573df01d4e846ba4a93934a0a
Matt Kunze <matt.kunze@datasplice.com>
2013-07-21 11:31:48 -0600

    file reorg
    moving files around so gh-pages can just be a branch of master and we
    don't need to do the squirelly deploy stuff

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var express = require('express')
 
 var app = express()
 
-app.use('/', express.static(__dirname + '/../www'))
-app.use('/course-content', express.static(__dirname + '/../../course-content'))
+app.use('/', express.static(__dirname))
+app.use('/course-content', express.static(__dirname + '/../course-content'))
 
 app.listen(3000)
 console.log('server listening on http://localhost:3000/')
